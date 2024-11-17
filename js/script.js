13857c9791bcbfaaaf5f8da1042336dcf1ac4110
KHOIDZ113 <khoit0000p@gmail.com>
2024-11-17 11:31:06 +0700

    message

--- js/script.js
+++ js/script.js
@@ -9,3 +9,10 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
         window.location.href = 'http://127.0.0.1:5500/bán%20acc.html#';
     }, 2000);
 });
+// Example of a simple JS function for button interaction
+document.querySelectorAll('.buy-btn').forEach(button => {
+    button.addEventListener('click', function() {
+        alert("Bạn đã chọn mua sản phẩm!");
+    });
+});
+
